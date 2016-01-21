df0dc0c1baa39f8ad8eae37f9a14d5cf1ff3615d
buschidos <sergey.snjozyk@gmail.com>
2016-01-21 18:14:11 +0200

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
+import $ from 'jquery';
+
 import './modules/menu';
+
+let clickCount = 0;
+
+$('.controll-btn-volume').on('click', function(e) {
+    let className = this.className;
+    this.className = className.replace(/level-\d/, `level-${++clickCount % 3}`);
+});
+
+$('.controll-btn-close').on('click', function() {
+    $('.modal-video').fadeOut(500);
+});
