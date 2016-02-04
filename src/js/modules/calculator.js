0898da3a286cd270b6b7d1f2de3f090de102d00c
buschidos <sergey.snjozyk@gmail.com>
2016-02-04 15:53:59 +0200

    catalog item page done

--- src/js/modules/calculator.js
+++ src/js/modules/calculator.js
@@ -3,7 +3,7 @@ import noUiSlider from 'nouislider';
 
 const $container = $('.calc');
 
-const $slider = $container.find('.calc__slider');
+const $slider = $container.find('.slider__base');
 
 if ($container.length) {
     noUiSlider.create($slider[0], {
@@ -12,5 +12,17 @@ if ($container.length) {
             min: 0,
             max: 100
         }
+        // pips: {
+        //     mode: 'steps',
+        //     density: 2
+        // }
+    });
+
+    $slider[0].noUiSlider.on('slide', function(valuesStr, handleIndex, valuesInt, what) {
+        $slider
+            .parent()
+            .find('.slider__value')[0]
+            .firstChild
+            .textContent = `${valuesInt[handleIndex].toFixed(1)} `;
     });
 }
