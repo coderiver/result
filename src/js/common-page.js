d7627a8a89ff7412259bdda0c3b77cf380fe0f68
buschidos <sergey.snjozyk@gmail.com>
2016-02-25 15:17:53 +0200

    catalog item gallery

--- src/js/common-page.js
+++ src/js/common-page.js
@@ -2,6 +2,7 @@
 import $ from 'jquery';
 import 'svgxuse';
 import 'gsap';
+import 'slick-carousel';
 import ScrollMagic from 'scrollmagic';
 
 import './modules/menu';
@@ -9,6 +10,7 @@ import './modules/calculator';
 
 import initSelectPlugin from './plugins/select';
 import navColorChange from './plugins/nav-color-change';
+import Gallery from './classes/gallery';
 
 initSelectPlugin($);
 $('.select').select();
@@ -62,6 +64,39 @@ navColorChange();
         });
 
     }).addTo(controller);
+
+})();
+
+(() => {
+
+    /**
+     * Topare slider for catalog item page
+     */
+
+    const toparea = $('.toparea');
+    if (!toparea.length) return;
+
+    const topareaGallery = new Gallery(toparea.find('.toparea-gallery'));
+    const navButtons = $('.select-buttons-group').first().find('.btn');
+    const title = toparea.find('.toparea__subtitle');
+
+    navButtons.on('click', function(e) {
+        const $this = $(this);
+        e.preventDefault();
+        topareaGallery.slickApiCall('slickGoTo', $this.index());
+    });
+
+    topareaGallery.getElement().on('beforeChange', (e, slick, current, next) => {
+        const clicked = navButtons.eq(next);
+
+        navButtons.filter('.is-selected').removeClass('is-selected');
+        clicked.addClass('is-selected');
+
+        title.fadeOut(400, () => {
+            title.text(clicked.data('text') || clicked.text());
+            title.fadeIn(400);
+        });
+    });
 
 })();
 
